c6951529486173f7e54da5be9080f6457d554290
palolik <azizulalamprottoy@gmail.com>
2024-09-08 16:58:17 +0600

    project setup

---
+++ app/src/main/java/com/kotlin5/expenses/data/data.kt
@@ -0,0 +1,11 @@
+package com.kotlin5.expenses.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "expenses")
+data class Expense(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val amount: Double
+)
