10b1c0bb8416cac588d81331ce4d8325e646dc33
palolik <azizulalamprottoy@gmail.com>
2024-09-15 13:05:55 +0600

    new pages added

---
+++ app/src/main/java/com/kotlin5/expenses/ui/view/TransactionActivity.kt
@@ -0,0 +1,15 @@
+package com.kotlin5.expenses.ui.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kotlin5.expenses.R
+
+class TransactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transaction)
+        }
+    }
